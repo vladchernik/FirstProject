6cdd128a974c287492c28c15e172154ebd121781
zhu4ara <157126983+vladchernik@users.noreply.github.com>
2026-03-12 13:36:00 +0300

    Update Simple.java

--- Simple.java
+++ Simple.java
@@ -2,6 +2,7 @@ import java.time.LocalTime;
 class Simple{
     public static void main (String args[]){
         System.out.println("Chernik Vladislav");
+        System.out.println("Hello");
         System.out.println(LocalTime.now());
     }
 }
